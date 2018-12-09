b2ad87caecd7b7c1a02d42c0ff4a8264b7cc851e
liangfung <1098486429@qq.com>
2018-12-09 23:04:55 +0800

    jsonp

---
+++ 1_jsonp/index.js
@@ -0,0 +1,31 @@
+/**
+ * @param {object} options
+ * @returns {Promise}
+ */
+var jsonp = function ({ cb, url, params }) {
+  return new Promise((resolve) => {
+    let script = document.createElement('script')  // 动态add  script元素
+    window[cb] = function (data) {
+      document.removeChild(script)
+      resolve(data)
+    }
+    let paramsArr = []
+    params = {
+      ...params,
+      cb
+    }
+    for (let key in params) {
+      paramsArr.push(`${key}=${params[key]}`)
+    }
+    script.src = `${url}?${paramsArr.join('&')}`
+    document.body.appendChild(script)
+  })
+}
+
+jsonp({
+  url: 'https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su',
+  cb: 'deliverer',  // 作用是 1.resolve promise (有闭包) 2.remove <script>标签
+  params: { wd: 'web' }
+}).then(data => {
+  console.log(data)
+})
---
+++ README.md
@@ -0,0 +1,15 @@
+# 9 ways to cross origin
+实现跨域的9种方法
+
+1. jsonp
+2. CORS
+3. document.domain
+4. postMessage
+5. window.name
+6. location.hash
+7. http-proxy
+8. nginx
+9. websocket
+
+## jsonp
+script标签的src获取资源，不受浏览器同源策略限制，可用
