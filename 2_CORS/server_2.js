d150166f023c3a97c39cc12598fb02fb41c3bf5b
liangfung <1098486429@qq.com>
2018-12-15 14:51:03 +0800

    cors

--- 2_CORS/server_2.js
+++ 2_CORS/server_2.js
@@ -1,4 +1,22 @@
 const express = require('express')
 const app = express()
 
+const whiteList = [
+  'http://localhost:3000'
+]
+
+app.use((req, res, next) => {
+  let { origin } = req.headers
+  console.log(req.headers)
+  if (whiteList.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin)
+  }
+  next()
+})
+
+app.get('/api/a', (req, res) => {
+  res.end('ok')
+})
+
 app.listen(8000)
+console.log('listen on port 8000')
